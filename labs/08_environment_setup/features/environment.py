21038fb5c9d690b1bcf6a3d02138b7227545914b
jxsaw <jxsaw@hotmail.com>
2024-11-05 12:42:59 -0500

    finish setup environement

--- labs/08_environment_setup/features/environment.py
+++ labs/08_environment_setup/features/environment.py
@@ -4,10 +4,13 @@ Environment for Behave Testing
 from os import getenv
 from selenium import webdriver
 
+BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
+WAIT_SECONDS = int(getenv("WAIT_SECONDS", '60'))
 
 def before_all(context):
     """ Executed once before all tests """
-
+    context.base_url = BASE_URL
+    context.wait_seconds = WAIT_SECONDS
 
 def after_all(context):
     """ Executed after all tests """
