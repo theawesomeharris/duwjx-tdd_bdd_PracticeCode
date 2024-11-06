bfe7079ceb743841859f79a9945aca6aa27f9246
jxsaw <jxsaw@hotmail.com>
2024-11-05 21:21:18 -0500

    updated web_steps with hints from Behave

--- labs/11_generating_steps/features/steps/web_steps.py
+++ labs/11_generating_steps/features/steps/web_steps.py
@@ -7,4 +7,36 @@ For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
 
-# Replace this comment with your web steps...
+@given(u'I am on the "Home Page"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given I am on the "Home Page"')
+
+
+@when(u'I set the "Category" to "dog"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I set the "Category" to "dog"')
+
+
+@when(u'I click the "Search" button')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I click the "Search" button')
+
+
+@then(u'I should see the message "Success"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should see the message "Success"')
+
+
+@then(u'I should see "Fido" in the results')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should see "Fido" in the results')
+
+
+@then(u'I should not see "Kitty" in the results')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should not see "Kitty" in the results')
+
+
+@then(u'I should not see "Leo" in the results')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should not see "Leo" in the results')
